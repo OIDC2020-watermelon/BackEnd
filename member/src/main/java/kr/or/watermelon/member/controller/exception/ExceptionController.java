84a8cbea10b678f3b3562eb47e5efee0b96fc69e
Hwamoc <hwamok.mocha@gmail.com>
2020-07-23 06:06:21 +0900

    feat: ⚡ add exception handling in spring security

---
+++ member/src/main/java/kr/or/watermelon/member/controller/exception/ExceptionController.java
@@ -0,0 +1,25 @@
+package kr.or.watermelon.member.controller.exception;
+
+import kr.or.watermelon.member.advice.exception.CAuthenticationEntryPointException;
+import kr.or.watermelon.member.model.response.CommonResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/exception")
+public class ExceptionController {
+
+    @GetMapping(value = "/entrypoint")
+    public CommonResult entrypointException() {
+        throw new CAuthenticationEntryPointException();
+    }
+
+    @GetMapping(value = "/accessdenied")
+    public CommonResult accessdeniedException() {
+        throw new AccessDeniedException("");
+    }
+}
