0c0ce87d4d5820bd16c76a57143829afb40bb11c
Hwamoc <hwamok.mocha@gmail.com>
2020-07-29 16:30:33 +0900

    fix: 🔥 Update modifying user information

--- member/src/main/java/kr/or/watermelon/member/entity/User.java
+++ member/src/main/java/kr/or/watermelon/member/entity/User.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 @Builder // builder를 사용할수 있게 합니다.
 @Entity // jpa entity임을 알립니다.
 @Getter // user 필드값의 getter를 자동으로 생성합니다.
+@Setter // user 필드값의 setter를 자동으로 생성합니다.
 @NoArgsConstructor // 인자없는 생성자를 자동으로 생성합니다.
 @AllArgsConstructor // 인자를 모두 갖춘 생성자를 자동으로 생성합니다.
 @Table(name = "user") // 'user' 테이블과 매핑됨을 명시
