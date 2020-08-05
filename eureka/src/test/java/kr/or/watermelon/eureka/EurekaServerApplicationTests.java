0a267ab753d1a583d0bbb40ff37922f4b63438e5
Hwamoc <hwamok.mocha@gmail.com>
2020-08-06 05:30:17 +0900

    feat: ⚡ Add eureka server

---
+++ eureka/src/test/java/kr/or/watermelon/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.watermelon.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
