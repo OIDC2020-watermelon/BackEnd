4154e3e5eaafcc3768e8ada1c18d9715b2102e72
Hwamoc <hwamok.mocha@gmail.com>
2020-08-06 23:52:27 +0900

    fix: 🔥 Drop conflict (duplicate) files

--- reservation/src/main/java/kr/or/watermelon/show/dto/ReqReleaseStatus.java
+++
@@ -1,9 +0,0 @@
-package kr.or.watermelon.show.dto;
-
-public enum ReqReleaseStatus {
-
-    BEFORE_RELEASE,
-    RELEASE_STARTED,
-    RELEASE_ENDED
-
-}
--- reservation/src/main/java/kr/or/watermelon/show/entity/CustomBuilder.java
+++
@@ -1,5 +0,0 @@
-package kr.or.watermelon.show.entity;
-
-public interface CustomBuilder {
-    Object build();
-}
