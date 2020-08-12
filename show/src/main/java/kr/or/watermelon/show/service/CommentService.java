8fdb6cbd16d28d78b22fc2e1c55fd462af4ea871
Hwamoc <hwamok.mocha@gmail.com>
2020-08-13 05:21:56 +0900

    feat: ⚡ Add to register, modify & delete comment api

--- show/src/main/java/kr/or/watermelon/show/service/CommentService.java
+++ show/src/main/java/kr/or/watermelon/show/service/CommentService.java
@@ -1,6 +1,8 @@
 package kr.or.watermelon.show.service;
 
 import kr.or.watermelon.show.dto.CommentDto;
+import kr.or.watermelon.show.dto.ReqCommentDto;
+import kr.or.watermelon.show.dto.UserIdDto;
 import kr.or.watermelon.show.entity.Comment;
 import kr.or.watermelon.show.entity.CommentType;
 import kr.or.watermelon.show.entity.Product;
@@ -11,7 +13,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,5 +31,39 @@ public class CommentService {
                 .map(c -> modelMapper.map(c, CommentDto.class))
                 .collect(Collectors.toList());
         return commentResponses;
+    }
+
+    public CommentDto registerComment(Long productId, UserIdDto user, CommentType type, ReqCommentDto reqCommentDto) {
+        Comment comment = Comment.builder()
+                .product(Product.builder().id(productId).build())
+                .userId(user.getId())
+                .type(type)
+                .content(reqCommentDto.getContent())
+                .createdDateTime(reqCommentDto.getCreatedDateTime())
+                .build();
+        Comment newComment = commentRepository.save(comment);
+        return modelMapper.map(newComment, CommentDto.class);
+    }
+
+    public CommentDto modifyComment(UserIdDto user, Long commentId, ReqCommentDto reqCommentDto) {
+        Comment comment = commentRepository.getOne(commentId);
+        if(!comment.getUserId().equals(user.getId())) { // 인증받은 댓글 수정 요청한 사용자와 댓글 작성자가 같은지 확인
+            throw new IllegalArgumentException("The current user cannot modify this commment");
+        }
+        comment.setUpdate(reqCommentDto.getContent(), reqCommentDto.getModifiedDateTime());
+        Comment modifiedComment = commentRepository.save(comment);
+        return modelMapper.map(modifiedComment, CommentDto.class);
+    }
+
+    public Map<String, String> deleteComment(UserIdDto user, Long commentId) {
+        Comment comment = commentRepository.getOne(commentId);
+        if(!comment.getUserId().equals(user.getId())) { // 인증받은 댓글 수정 요청한 사용자와 댓글 작성자가 같은지 확인
+            throw new IllegalArgumentException("The current user cannot delete this commment");
+        }
+        Map<String, String> response = new HashMap<String, String>();
+        commentRepository.deleteById(commentId);
+        response.put("result", "200");
+        response.put("msg", "ok");
+        return response;
     }
 }
