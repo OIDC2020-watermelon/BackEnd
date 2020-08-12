b4e074816126cc78181fd79ce054e2489c1ffc33
Hwamoc <hwamok.mocha@gmail.com>
2020-08-13 04:35:42 +0900

    feat: ⚡ Add feign config

--- show/src/main/java/kr/or/watermelon/show/ShowApplication.java
+++ show/src/main/java/kr/or/watermelon/show/ShowApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+
 @EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
 public class ShowApplication {
 
     public static void main(String[] args) {
@@ -17,7 +21,7 @@ public class ShowApplication {
 
     @Bean
     @LoadBalanced
-    public RestTemplate getRestTemplate() {
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 }
