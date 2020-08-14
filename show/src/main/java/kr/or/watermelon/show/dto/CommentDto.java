4474997b953934c22d56607c454f84fedecd711f
changgunyee <minyoung403@naver.com>
2020-08-14 15:55:35 +0900

    fix: :zap: add commetId for deleting in front

--- show/src/main/java/kr/or/watermelon/show/dto/CommentDto.java
+++ show/src/main/java/kr/or/watermelon/show/dto/CommentDto.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class CommentDto {
+    private Long id;
     private Long userId;
     private String content;
     private LocalDateTime createdDateTime;
