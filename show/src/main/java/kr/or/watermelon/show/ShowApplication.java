d31f2d43ba55fd9e4f380fbc2fc758db9f6e2d24
changgunyee <minyoung403@naver.com>
2020-08-16 16:55:28 +0900

    chore: 📦 add config for batch and scheduler

--- show/src/main/java/kr/or/watermelon/show/ShowApplication.java
+++ show/src/main/java/kr/or/watermelon/show/ShowApplication.java
@@ -1,15 +1,17 @@
 package kr.or.watermelon.show;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
-
+@EnableScheduling
+@EnableBatchProcessing
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
