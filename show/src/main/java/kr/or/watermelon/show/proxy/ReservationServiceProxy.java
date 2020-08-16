2bec7dcb4dfae8b2134a8ffeaf061b79d039e857
changgunyee <minyoung403@naver.com>
2020-08-16 19:13:35 +0900

    fix: :fire: add removed package import

--- show/src/main/java/kr/or/watermelon/show/proxy/ReservationServiceProxy.java
+++ show/src/main/java/kr/or/watermelon/show/proxy/ReservationServiceProxy.java
@@ -3,6 +3,8 @@ package kr.or.watermelon.show.proxy;
 import kr.or.watermelon.show.dto.PerformanceDto;
 import kr.or.watermelon.show.dto.PerformanceInfoDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
